551d6fa40c7dfc1c9928237b1a5b820f42ea6b2f
A-Mosher <adam.l.mosher@gmail.com>
2021-05-05 22:14:43 -0400

    Finally fixed my goToNextCard and goToPreviousCard functions in app.jsx for and utilized them in a buttons to scroll through cards. also added CardViewer folder with cardViewer jsx and css files.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -16,6 +16,11 @@ class App extends Component {
                     question: 'What is Captain America\'s shield made out of?',
                     answer: 'Vibranium'
                 },
+                {
+                    category: 'Marvel',
+                    question: 'What type of doctor is Stephen Strange?',
+                    answer: 'Neurosurgeon',
+                },
             ]
             },
 
@@ -30,30 +35,72 @@ class App extends Component {
                     question: 'What does IPA stand for?',
                     answer: 'India Pale Ale',
                 },
+                {
+                    category: 'Beer',
+                    question: 'what are the 4 ingredients necessary for brewing beer?',
+                    answer: 'Water, Malt, Hops, Yeast',
+                },
             ]
             },
         ];
-        
+
         this.state = {
             collectionNumber: 0,
             cardNumber: 0,
         } 
     }
-    state = { }
+
+    goToNextCard = () =>{
+        if(this.state.cardNumber < this.collection[this.state.collectionNumber].cards.length - 1){
+            this.setState({
+                cardNumber: this.state.cardNumber + 1,
+            });
+        }
+        else{
+            this.setState({
+                cardNumber: 0,
+            });
+        }
+    }
+
+    // goToNextCard(){
+    //     let tempCardNumber = this.state.cardNumber;
+    //     tempCardNumber++;
+    //     if(tempCardNumber === this.collection.length + 1){
+    //         tempCardNumber = 0;
+    //     }
+    //     this.setState({
+    //         cardNumber: tempCardNumber
+    //     });
+    // }
+
+    goToPreviousCard(){
+        if(this.state.cardNumber > 0){
+            this.setState({
+                cardNumber: this.state.cardNumber - 1,
+            });
+        }
+        else{
+            this.setState({
+                cardNumber: this.collection[this.state.collectionNumber].cards.length-1,
+            });
+        }
+    }
+
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
                 <div className="row row-spacer">
                     <div className="col">
-                        {}
+                    <button onClick={() => this.goToPreviousCard()}>Previous Card</button>
                     </div>
                     <div className="col">
                         <h1>{this.collection[this.state.collectionNumber].title}</h1>
                         <h4>{this.collection[this.state.collectionNumber].cards[this.state.cardNumber].question}</h4>
                     </div>
                     <div className="col">
-                        {}
+                        <button onClick={() => this.goToNextCard()}>Next Card</button>
                     </div>
                 </div>
             </div>
